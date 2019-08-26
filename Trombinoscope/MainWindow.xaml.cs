19e614ca3beace5a3d133184a2b4b7b962a554c0
dialloi659 <44133766+dialloi659@users.noreply.github.com>
2019-08-26 12:57:23 +0200

    Ajoutez des fichiers projet.

---
+++ Trombinoscope/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trombinoscope
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<GetAllContactsResult> Contacts { get; set; }
+
+        public MainWindow()
+        {            
+            var MyCont = new TrombiDataClasses1DataContext();
+
+            long StartBytes = System.GC.GetTotalMemory(true);
+            Contacts = MyCont.GetAllContacts().ToList();
+            long StopBytes = System.GC.GetTotalMemory(true);
+            var delta = StopBytes - StartBytes;
+            this.DataContext = this;
+            InitializeComponent();
+            
+        }
+    }
+}
